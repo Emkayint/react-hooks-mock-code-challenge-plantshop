fc400a228a284ba7b58c19053d64181ebd104ba3
Martin <mkmartinoes@gmail.com>
2022-05-08 18:59:02 +0300

    fetching data

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+  const [plants, setPlants] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3000/plants')
+    .then(res => res.json())
+    .then(res => setPlants(plants => plants = res))
+  }, [])
+
+  
+
   return (
     <ul className="cards">{/* render PlantCards components in here */}</ul>
   );
