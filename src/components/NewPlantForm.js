4c68d7245f2710216a72f1c0b8b2880da439f6cc
Martin <mkmartinoes@gmail.com>
2022-05-08 20:11:18 +0300

    Handle post

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -7,22 +7,36 @@ function NewPlantForm() {
     price : ''
   })
 
-  function handleSubmit(){
-    return fetch('http://localhost:3000/plants', {
+  function handleChange(e){
+    setFormData({
+      ...formData,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData)
+    fetch("http://localhost:3000/plants", {
       method : "POST",
       headers : {
         "Content-Type" : "application/json"
       },
-      data : JSON.stringify(formData)
+      body : JSON.stringify({
+        name : formData.name,
+        image : formData.image,
+        price : formData.price
+      })
     })
   }
+  
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form onClick={ handleSubmit }>
-        <input type="text" name="name" placeholder="Plant name" value={ formData.name } />
-        <input type="text" name="image" placeholder="Image URL" value = { formData.image}/>
-        <input type="number" name="price" step="0.01" placeholder="Price"  value = {formData.price}/>
+      <form onSubmit={ handleSubmit }>
+        <input type="text" name="name" placeholder="Plant name" value={ formData.name } onChange= {handleChange}/>
+        <input type="text" name="image" placeholder="Image URL" value = { formData.image} onChange = { handleChange }/>
+        <input type="number" name="price" step="0.01" placeholder="Price"  value = {formData.price} onChange = {handleChange}/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
